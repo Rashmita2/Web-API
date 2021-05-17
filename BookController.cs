fc7487e8f01833aa3aa02df4e345b715287f4dcd
Rashmita Khatri <67445848+Rashmita2@users.noreply.github.com>
2021-05-17 17:43:13 -0500

    Create BookController.cs

---
+++ BookController.cs
@@ -0,0 +1,41 @@
+using BookStore.Data.Interfaces;
+using BookStore.Data.Models;
+using BookStore.Data.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BookRepository books = new BookRepository();
+       [HttpGet]
+       public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return books.GetAllBooks();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+
+            // var book = books.FirstOrDefault(x => x.ID == id);
+            var book = books.GetBook(id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+       
+    }
+}
+
